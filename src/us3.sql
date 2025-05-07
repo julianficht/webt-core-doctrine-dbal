66e56f7d4dc8dfe4741ee6efbbca8286f9f206a4
OriHatza <1128@htl.rennweg.at>
2025-05-07 11:04:39 +0200

    Change User Story 3

--- src/us3.sql
+++ src/us3.sql
@@ -1,6 +1,7 @@
 INSERT INTO game_rounds (player_name, move, played_at) VALUES
-('Alice',    'rock',     '2025-05-07 14:00:00'),
-('Bob',      'paper',    '2025-05-07 14:02:00'),
-('Charlie',  'scissors', '2025-05-07 14:04:00'),
-('Dana',     'rock',     '2025-05-07 14:06:00'),
-('Eli',      'scissors', '2025-05-07 14:08:00');
+('Andi',    'rock',     '2025-05-07 14:00:00'),
+('Nico',      'paper',    '2025-05-07 14:02:00'),
+('Moe',  'scissors', '2025-05-07 14:04:00'),
+('Julian',     'rock',     '2025-05-07 14:06:00'),
+('Tobi',      'scissors', '2025-05-07 14:08:00');
+('Daniel', 'rock', '2025-05-07 14:08:00');
