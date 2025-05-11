8e3621657fd3ec75d9e21270d7ec3d853f7962df
OriHatza <1128@htl.rennweg.at>
2025-05-11 12:09:50 +0200

    user story 3 final
    2 spieler pro runde statt 1

--- src/us3.sql
+++ src/us3.sql
@@ -1,8 +1,30 @@
-INSERT INTO game_rounds (player_name, move, played_at) VALUES
-('Andi',    'rock',     '2025-05-07 14:00:00'),
-('Nico',      'paper',    '2025-05-07 14:02:00'),
-('Moe',  'scissors', '2025-05-07 14:04:00'),
-('Julian',     'rock',     '2025-05-07 14:06:00'),
-('Tobi',      'scissors', '2025-05-07 14:08:00'),
-('Professor',      'rock', '2025-05-07 14:12:00'),
-('Daniel', 'rock', '2025-05-07 14:08:00');
+
+-- Runde 1
+INSERT INTO game_rounds (played_at) VALUES ('2025-05-07 14:00:00');
+INSERT INTO game_moves (round_id, player_name, move) VALUES
+                                                         (1, 'Andi', 'rock'),
+                                                         (1, 'Nico', 'paper');
+
+-- Runde 2
+INSERT INTO game_rounds (played_at) VALUES ('2025-05-07 14:05:00');
+INSERT INTO game_moves (round_id, player_name, move) VALUES
+                                                         (2, 'Moe', 'scissors'),
+                                                         (2, 'Julian', 'rock');
+
+-- Runde 3
+INSERT INTO game_rounds (played_at) VALUES ('2025-05-07 14:10:00');
+INSERT INTO game_moves (round_id, player_name, move) VALUES
+                                                         (3, 'Tobi', 'scissors'),
+                                                         (3, 'Professor', 'rock');
+
+-- Runde 4
+INSERT INTO game_rounds (played_at) VALUES ('2025-05-07 14:15:00');
+INSERT INTO game_moves (round_id, player_name, move) VALUES
+                                                         (4, 'Daniel', 'rock'),
+                                                         (4, 'Sabrina', 'paper');
+
+-- Runde 5
+INSERT INTO game_rounds (played_at) VALUES ('2025-05-07 14:20:00');
+INSERT INTO game_moves (round_id, player_name, move) VALUES
+                                                         (5, 'Lea', 'scissors'),
+                                                         (5, 'Ben', 'scissors');
