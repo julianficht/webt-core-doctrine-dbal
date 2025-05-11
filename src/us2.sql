c6f6adacf725cb399b65d575d7ec746bcc838376
OriHatza <1128@htl.rennweg.at>
2025-05-11 12:08:01 +0200

    user story 2 final
    2 spieler am zug pro runde statt 1

--- src/us2.sql
+++ src/us2.sql
@@ -1,6 +1,12 @@
 CREATE TABLE game_rounds (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    player_name VARCHAR(255) NOT NULL,
-    move ENUM('rock', 'paper', 'scissors') NOT NULL,
-    played_at DATETIME NOT NULL
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             played_at DATETIME NOT NULL
+);
+
+CREATE TABLE game_moves (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            round_id INT NOT NULL,
+                            player_name VARCHAR(255) NOT NULL,
+                            move ENUM('rock', 'paper', 'scissors') NOT NULL,
+                            FOREIGN KEY (round_id) REFERENCES game_rounds(id) ON DELETE CASCADE
 );
